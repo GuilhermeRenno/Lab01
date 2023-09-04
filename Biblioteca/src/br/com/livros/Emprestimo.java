78384175e23a25b4eea2a4423780f22e178560db
Guilherme Renno <guilherme.silveirareno@gmail.com>
2023-09-04 09:40:57 -0300

    get e sets classe emprestimo

--- Biblioteca/src/br/com/livros/Emprestimo.java
+++ Biblioteca/src/br/com/livros/Emprestimo.java
@@ -1,5 +1,26 @@
 package br.com.livros;
 
 public class Emprestimo {
-
+		private Livro livro;
+		private String dataEmprestimo;
+		private String dataDevolucao;
+		
+		public Livro getLivro() {
+			return livro;
+		}
+		public void setLivro(Livro livro) {
+			this.livro = livro;
+		}
+		public String getdataEmprestimo() {
+			return dataEmprestimo;
+		}
+		public void setdataEmprestimo(String dataEmprestimo){
+			this.dataEmprestimo = dataEmprestimo;
+		}
+		public String getdataDevolucao() {
+			return dataDevolucao;
+		}
+		public void setdataDevolucao(String dataDevolucao) {
+			this.dataDevolucao = dataDevolucao;
+		}
 }
