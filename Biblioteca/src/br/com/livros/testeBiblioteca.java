d5cb78105a905679d7bd1b312827eb0491b1f81f
Guilherme Renno <guilherme.silveirareno@gmail.com>
2023-09-04 20:26:17 -0300

    finalAula04/09

--- Biblioteca/src/br/com/livros/testeBiblioteca.java
+++ Biblioteca/src/br/com/livros/testeBiblioteca.java
@@ -1,18 +1,34 @@
 package br.com.livros;
 
+import java.time.LocalDate;
+
 //import java.time.format.DateTimeFormatter;
 
 public class testeBiblioteca {
-
-	public void main(String[]args) {
+//funcao metodo principal
+	public static void main(String[]args) {
 		Livro livro1 = new Livro();
 		livro1.construtorLivro(0001, "Alto da compadecida", "Ariano Suasuna", false); 
+		Emprestimo emprestimo1 = new Emprestimo();
+		emprestimo1.construtor(livro1,LocalDate.now(),LocalDate.now().plusDays(7));
+		
+		
 		Livro livro2 = new Livro();
 		livro2.construtorLivro(0001, "Alto da compadecida", "Edua", false); 
+		Emprestimo emprestimo2 = new Emprestimo();
+		emprestimo2.construtor(livro2,LocalDate.now(),LocalDate.now().plusDays(7));
+		livro1.emprestar();
+		System.out.println("Dias restantes para devolucao do livro "+ emprestimo1.getdataDevolucao());	
+		livro2.emprestar();
+		System.out.println("Dias restantes para devolucao do livro "+ emprestimo2.getdataDevolucao());
+		
+		emprestimo1.getLivro().devolver();
+		 System.out.println("Dias restantes para devolução : " + emprestimo2);
+	     System.out.println("Emprestimo está atrasado? " + emprestimo2.atrasado());
 	}
-}
+	}
 
-
+//
 
 
 //DateTimeFormatter formatadorBarra = DateTimeFormatter.ofPattern("dd/mm/yyyy");
